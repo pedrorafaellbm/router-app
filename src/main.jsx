ddb5ef7695f11de8a4a4f7a0379e22b156a086db
PEDRO RAFAEL LIMA BEZERRA DE MEDEIROS <pedromedeiros0978@digitalcollege.internal>
2025-09-02 21:07:26 -0300

    intervalo

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'bootstrap/dist/scss/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/Login.jsx';
+import { Cadastro } from './pages/Cadastro.jsx';
+import { NotFound } from './pages/NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <App /> */}
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/cadastro' element={<Cadastro />} />
+        {/* {404} */}
+        <Route path='*' element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
