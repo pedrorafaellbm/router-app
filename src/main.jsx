e75bf19f2d4451db8337e9f6851f5c08b61df893
PEDRO RAFAEL LIMA BEZERRA DE MEDEIROS <pedromedeiros0978@digitalcollege.internal>
2025-09-02 22:00:09 -0300

    cadastro

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,33 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
 import { Login } from './pages/Login.jsx';
 import { Cadastro } from './pages/Cadastro.jsx';
 import { NotFound } from './pages/NotFound.jsx';
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login/>
+  },
+  {
+    path: '/home',
+    element: <App/>
+  },
+  {
+    path: '/cadastro',
+    element: <Cadastro/>
+  },
+  {
+    path: '*',
+    element: <NotFound/>
+  },
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      {/* <App /> */}
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/cadastro' element={<Cadastro />} />
-        {/* {404} */}
-        <Route path='*' element={<NotFound />} />
-
-      </Routes>
-    </BrowserRouter>
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
